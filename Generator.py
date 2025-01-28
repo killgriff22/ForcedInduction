b7b4224ad2ad2a08d8d151ac06cd4d38d3561c39
killgriff22 <kilgrif22033Gmail.com>
2025-01-27 23:56:26 -0500

    push all my changes none of these are on steam

--- Generator.py
+++ Generator.py
@@ -1,28 +1,113 @@
-import pygame
+from tkinter import *
 import os
 import sys
 from utils import *
 os.chdir(os.path.dirname(__file__))
-window = pygame.display.set_mode((1500, 800))
 mapfile = open("map0.csv", "r")
-map = mapfile.read().split("\n")
+_map = mapfile.read().split("\n")
 mapfile.close()
-str().isdecimal
-map = [list(map[i].split(",")) for i in range(len(map))]
-map = [[int(map[i][j])for j in range(len(map[i]))] for i in range(len(map))]
-
-pygame.font.init()
-font = pygame.font.Font(None, 30)
-pygame.init()
-
-selected_tile = None
-buttons = []
-
+root = Tk()
+_map = [list(_map[i].split(",")) for i in range(len(_map))]
+_map = [[int(_map[i][j])for j in range(len(_map[i]))] for i in range(len(_map))]
+map_metafile = open("map0_meta.csv", "r")
+map_meta = map_metafile.read().split("\n")
+map_metafile.close()
+map_meta = [i.split(",") for i in map_meta]
+map_reprs = []
+availible_Inputs = [
+    'RPM',
+    'Power',
+    'Torque',
+    'Vol. Eff.',
+    'throttle angle',
+    'Fuel Flow rate',
+    'intake flow rate',
+]
+availible_Outputs = [
+    'Peak Power %',
+    'Peak Power RPM',
+    'Throttle Idle Min angle',
+    'Throttle Idle RPM',
+    'Max RPM',
+    'Fuel Ratio',
+    'Exhaust Effect'
+]
+def Return(event):
+    if event.char == '\r':
+        update()
+        done= False
+        for i in range(len(map_reprs)):
+            for j in range(len(map_reprs[i])):
+                if map_reprs[i][j] == root.focus_get():
+                    map_reprs[i][j+1 if j+1 < len(_map[i]) else 0].focus_set()
+                    done=True
+                    break
+                if done:
+                    break
+def key(event):
+    done = False
+    x,y = 0,0
+    for i in range(len(map_reprs)):
+        for j in range(len(map_reprs[i])):
+            if map_reprs[i][j] == root.focus_get():
+                done = True
+                x,y = i,j
+                break
+        if done:
+            break
+    match event.keycode:
+        case 38: #up
+            if x > 0:
+                x-=1
+            else:
+                x=len(map_reprs)-1
+        case 37: #left
+            if y > 0:
+                y-=1
+            else:
+                y=len(map_reprs[x])-1
+        case 40: #down
+            if x < len(map_reprs)-1:
+                x+=1
+            else:
+                x=0
+        case 39: #right
+            if y < len(map_reprs[x])-1:
+                y+=1
+            else:
+                y=0
+    map_reprs[x][y].focus_set()
+def update(first=False, window=root):
+    global _map
+    if not first:
+        #update map
+        _map = [[int(map_reprs[i][j].get()) for j in range(len(map_reprs[i]))] for i in range(len(map_reprs))]
+        minval = min([min(i) for i in _map])
+        maxval = max([max(i) for i in _map])
+        for i in range(len(_map)): #Rows
+            for j in range(len(_map[0])): #Columns
+                b=map_reprs[i][j]
+                b.config(bg=rgb_to_hex(interpolate_color(_map[i][j],minval,maxval)))  # Red
+                b.delete(0, END)
+                b.insert(0, str(_map[i][j]))
+    else:
+        #populate map
+        minval = min([min(i) for i in _map])
+        maxval = max([max(i) for i in _map])
+        for i in range(len(_map)): #Rows
+            map_reprs.append([])
+            for j in range(len(_map[0])): #Columns
+                b = Entry(window, text="",width=10)
+                b.grid(row=i, column=j)
+                map_reprs[i].append(b)
+                b.config(bg=rgb_to_hex(interpolate_color(_map[i][j],minval,maxval)))  # Red
+                b.delete(0, END)
+                b.insert(0, str(_map[i][j]))
 def save():
-    #process map for lua mod
+    _map = [[int(map_reprs[i][j].get()) for j in range(len(map_reprs[i]))] for i in range(len(map_reprs))]
     tunefile = open("main.lua", "w")
     tunefile.write(luatemplate1)
-    for i, row in enumerate(map):
+    for i, row in enumerate(_map):
         tunefile.write(f"[{i}] ={"{"}")
         for value in row:
             tunefile.write(f"{value},")
@@ -38,67 +123,55 @@ def save():
     tunefile.write("['scales'] = {")
     tunefile.write(metainfo[0][3]+","+metainfo[0][4]+","+metainfo[0][5]+"},")
     tunefile.write(midportion)
-    tunefile.write(f"['size'] = {{{len(map[0])}, {len(map)}}}")
+    tunefile.write(f"['size'] = {{{len(_map[0])}, {len(_map)}}}")
     tunefile.write(tailend)
     tunefile.close()
-    #process map out to csv
     mapfile = open("map0.csv", "w")
-    for y,row in enumerate(map):
-        for i,value in enumerate(row):
-            mapfile.write(str(value)+("," if not i == len(row)-1 else ""))
-        if not y == len(map)-1:
-            mapfile.write("\n")
+    mapfile.write("\n".join([",".join([str(j) for j in i]) for i in _map]))
     mapfile.close()
-"""
-['inputs'] = {
-['outputs'] = {
-['scales'] = {
-"""
-buttons.append(button(1200, 100, 100, 50, "Save", (0, 255, 0), save))
-
-
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == pygame.MOUSEBUTTONUP:
-            x = event.pos[0] // tile_size
-            y = event.pos[1] // tile_size
-            if x < len(map[0]) and y < len(map):
-                selected_tile = (x, y)
-            for button in buttons:
-                if button.is_over(event.pos):
-                    button.function()
-
-        elif event.type == pygame.KEYDOWN:
-            #if the key is a number and we have a selected tile, append the number to the value of the tile
-            if event.unicode.isdigit() and selected_tile:
-                map[selected_tile[1]][selected_tile[0]] = map[selected_tile[1]][selected_tile[0]] * 10 + int(event.unicode)
-            if event.key == pygame.K_BACKSPACE and selected_tile:
-                map[selected_tile[1]][selected_tile[0]] = map[selected_tile[1]][selected_tile[0]] // 10
-    window.fill((0, 0, 0))
-    average_tile_value = sum([sum(i) for i in map]) / (len(map) * len(map[0]))
-    for button in buttons:
-        button.draw(window)
-        if button.is_over(pygame.mouse.get_pos()):
-            button.color = (255, 0, 0)
-        else:
-            button.color = (0, 255, 0)
-    tile_size = 50
-    min_value = min(min(row) for row in map)
-    max_value = max(max(row) for row in map)
-
-    for y in range(len(map)):
-        for x in range(len(map[y])):
-            distance = map[y][x]
-            color = interpolate_color(distance, min_value, max_value)
-            #draw the value of the tile on the tile
-            text = font.render(str(distance), True, (0, 0, 0))
-            pygame.draw.rect(window, color, (x * tile_size, y * tile_size, tile_size, tile_size))
-            window.blit(text, (x * tile_size, y * tile_size))
-    #draw a hollow box with edgewidth 5 around the selected tile
-    if selected_tile:
-        pygame.draw.rect(window, (0, 0, 255), (selected_tile[0] * tile_size, selected_tile[1] * tile_size, tile_size, tile_size), 5)
-    pygame.display.flip()
+Button(root, text="Save", command=save).grid(row=len(map_reprs), column=0)
+selected_option = StringVar(root)
+Xaxis_dropdown = OptionMenu(root, selected_option, availible_Inputs[int(map_meta[0][0])], *availible_Inputs)
+Yaxis_dropdown = OptionMenu(root, selected_option, availible_Inputs[int(map_meta[0][1])], *availible_Inputs)
+Output_dropdown = OptionMenu(root, selected_option, availible_Outputs[int(map_meta[0][2])], *availible_Outputs)
+X_Scale = Entry(root, text="",width=10)
+Y_Scale = Entry(root, text="",width=10)
+Output_Scale = Entry(root, text="",width=10)
+labels = [
+    Label(root, text="X-Axis"),
+    Label(root, text="Y-Axis"),
+    Label(root, text="Output"),
+    Label(root, text="X-Scale"),
+    Label(root, text="Y-Scale"),
+    Label(root, text="Output-Scale")
+]
+metainfo  = [
+    Xaxis_dropdown,
+    Yaxis_dropdown,
+    Output_dropdown,
+    X_Scale,
+    Y_Scale,
+    Output_Scale
+]
+Xaxis_dropdown.grid(row=2, column=1)
+Yaxis_dropdown.grid(row=2, column=2)
+Output_dropdown.grid(row=2, column=3)
+X_Scale.grid(row=2, column=4)
+Y_Scale.grid(row=2, column=5)
+Output_Scale.grid(row=2, column=6)
+labels[0].grid(row=1, column=1)
+labels[1].grid(row=1, column=2)
+labels[2].grid(row=1, column=3)
+labels[3].grid(row=1, column=4)
+labels[4].grid(row=1, column=5)
+labels[5].grid(row=1, column=6)
+root.geometry("1500x800")
+def open_map_window():
+    window2 = Toplevel(root)
+    window2.title("Map 0")
+    update(True, window2)
+    
+root.bind_all('<Return>', Return)
+root.bind_all('<Key>', key)
+root.after(1000,open_map_window)
+root.mainloop()
--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-import pygame
 from templates import *
 def interpolate_color(value, min_value, max_value):
     # Normalize the value to be between 0 and 1
@@ -16,25 +15,5 @@ def interpolate_color(value, min_value, max_value):
         b = 0
     return (r, g, b)
 
-
-class button:
-    def __init__(self, x, y, width, height, text, color, function):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.text = text
-        self.color = color
-        self.function = function
-
-    def draw(self, window):
-        pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.height))
-        font = pygame.font.Font(None, 30)
-        text = font.render(self.text, True, (0, 0, 0))
-        window.blit(text, (self.x + self.width // 2 - text.get_width() // 2, self.y + self.height // 2 - text.get_height() // 2))
-
-    def is_over(self, pos):
-        if pos[0] > self.x and pos[0] < self.x + self.width:
-            if pos[1] > self.y and pos[1] < self.y + self.height:
-                return True
-        return False
+def rgb_to_hex(rgb):
+    return '#%02x%02x%02x' % rgb
